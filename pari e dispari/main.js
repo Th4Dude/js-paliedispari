85940b214a5061810b231652ebd054da9f634f37
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-01 19:54:22 +0200

    pari o dispari done

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,25 +1,40 @@
 'use strict'
 
+/* scelta utente pari o dispari */
 
+const utentePrimaScelta = prompt('scegli pari o dispari');
+console.log(utentePrimaScelta)
 
+/* scelta numero utente */
 
-function pariedispari (){
-    const utentePrimaScelta = prompt('scegli pari o dispari');
-    console.log(utentePrimaScelta)
-    const utenteSecondaScelta = Number(prompt('scegli un numero da 1 a 5'));
-    console.log(utenteSecondaScelta)
-    let numeroRandom = Math.floor(Math.random() * 5) + 1;
-    console.log(numeroRandom)
-    let totale = (utenteSecondaScelta + numeroRandom);
-    console.log(totale)
-    return totale
+const utenteSecondaScelta = Number(prompt('scegli un numero da 1 a 5'));
+console.log(utenteSecondaScelta)
+
+/* funzione numero random dal computer */
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+let numeroRandom = getRndInteger(1,5);
+
+/* somma */
+
+let totale = (utenteSecondaScelta + numeroRandom);
+console.log(totale)
+
+/* funzione */
+
+function pariodispari(nonsisa) {
+    return nonsisa % 2 === 0;
 }
 
-pariedispari ();
+let pari = pariodispari(totale);
 
-function pariedispari2 (){
-
-
+if ((utentePrimaScelta === "pari" && pari) || (utentePrimaScelta === "dispari" && !pari)) {
+    console.log("scelta giusta il numero e' " + utenteSecondaScelta + ", il numero del computer e' " + numeroRandom + ", totale " + totale);
+} else {
+    console.log("scelta sbagliata il numero e' " + utenteSecondaScelta + ", il numero del computer e' " + numeroRandom + ", totale " + totale);
 }
 
 
